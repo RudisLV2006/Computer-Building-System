8aed6006afa08925a529bd73b1587a13a9c101ae
Kristaps <kristapskakilns@gmail.com>
2025-12-09 14:47:07 +0200

    Fix test

--- Building-System/tests/Unit/BuildTest.php
+++ Building-System/tests/Unit/BuildTest.php
@@ -8,19 +8,17 @@ use App\Build;
 uses(Tests\TestCase::class);
 
 it('can load product and its specs with type', function () {
-    // Refresh the application to ensure Faker is properly initialized
     $this->artisan('migrate:fresh');
-    // Create a CPU with associated product
+    
     $cpu = CPUSpec::factory()->create();
     
     $build = new Build(null);
     $build->addItem('cpu', $cpu->product_id);
-    // var_dump($build);
     
-    // Call the loadModel method
     $result = $build->loadModel('cpu');
     
-    // Assert the result
-    expect($result)->toBeNull();
-    // expect($result)->toHaveKey('product');
+    expect($result)->not()->toBeNull();
+    
+    // Convert to array to check for 'product' key
+    expect($result)->toHaveKey('product');
 });
