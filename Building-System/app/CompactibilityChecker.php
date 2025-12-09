5114e2c8c9ca44f9d1d39e233e8bf6b5e461ccef
Kristaps <kristapskakilns@gmail.com>
2025-12-09 13:25:27 +0200

    Make type key-pair generator; Dynamic error message generator

--- Building-System/app/CompactibilityChecker.php
+++ Building-System/app/CompactibilityChecker.php
@@ -40,6 +40,7 @@ class CompactibilityChecker
     }
     public function validateBuild(){
         $errors = [];
+        $checkedPairs = [];
         foreach ($this->build->getItems() as $type => $item){
             // \Log::debug($type);
             if(!isset(self::$rules[$type])){
@@ -52,21 +53,73 @@ class CompactibilityChecker
                 if(!$this->build->hasItem($requiredType)){
                     continue;
                 }
+
+                $pairKey = $this->createKeyPairs($type, $requiredType, $rule['field'], $rule['match_field']);
+
+                if (isset($checkedPairs[$pairKey])) {
+                    continue;
+                }
+
+                $checkedPairs[$pairKey] = true;
+
                 $requiredValue = $this->build->getField($requiredType, $rule['match_field']);
                 $currentValue = $item['spec'][$rule['field']];
-                if(!($requiredValue === $currentValue)){
+                $operator = $rule['operator'] ?? '=';
+                if(!$this->checkCompactibility($requiredValue,$currentValue,$operator)){
                     // \Log::info("There is an error");
                     $errors[] = [
                         'component' => $type,
                         'component_name' => $this->build->getProduct($type)['name'],
                         'incompatible_with' => $requiredType,
                         'incompatible_name' => $this->build->getProduct($requiredType)['name'],
-                        'message' => "There are error",
+                        'field' => $rule['field'],
+                        'match_field' => $rule['match_field'],
+                        'expected' => $requiredValue,
+                        'actual' => $currentValue,
+                        'message' => $this->generateErrorMessage(
+                            $type,
+                            $rule,
+                            $requiredValue,
+                            $currentValue,
+                            $this->build->getProduct($type)['name'],
+                            $this->build->getProduct($requiredType)['name']
+                        ),
                     ];
                 }
             }
         }
         \Log::debug($errors);
         return $errors;
+    }
+    public function createKeyPairs($type1, $type2, $field1, $field2){
+        $component = [$type1,$type2];
+        sort($component);
+
+        if($field1===$field2){
+            return implode("-", $component) . ":" . $field1;
+        }
+        return implode("-", $component) . ":" . $field1 . "-" . $field2;
+    }
+    public function checkCompactibility($currentField, $requiredField,$operator){
+        switch($operator){
+            case "=":
+                return $currentField==$requiredField;
+            default:
+                return false;
+        }
+    }
+    public function generateErrorMessage($type, $rule, $requiredValue, $currentValue, $componentName, $requiredComponentName){
+        $fieldName = ucfirst(str_replace('_', ' ', $rule["field"]));
+
+        return sprintf(
+            "%s is incompatible with %s. %s requires %s %s but %s has %s.",
+            $componentName,
+            $requiredComponentName,
+            $requiredComponentName,
+            $fieldName,
+            $requiredValue,
+            $componentName,
+            $currentValue
+        );
     }
 }
